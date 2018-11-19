7ef9f90c71d9453217961f9fd3cce77cf94a696a
sho9377 <sho9377@bths.edu>
2018-11-19 14:44:22 -0500

    Add .gitignore file.

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        int[] arr1 = {328, 812, 1019, 519, 1011, 408, 406, 214, 1004, 222, 102, 816, 206, 601, 312, 713, 604};
+        Sorts.printArr(arr1);
+        System.out.println();
+    }
+}
